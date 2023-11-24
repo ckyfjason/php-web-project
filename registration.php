0889e498f3c8f32736d3a93e1ecee58ac77869e5
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-24 23:06:56 +0800

    .test

--- registration.php
+++ registration.php
@@ -5,7 +5,24 @@
 </head>
 <body>
 <?php
-require('db.php');
+$serverName = "streamweb-dbserver.database.windows.net"; // 資料庫伺服器名稱
+$username = "ckyfjason"; // 資料庫使用者名稱
+$password = "Database@password"; // 資料庫密碼
+$dbname = "streamweb-formaldb"; // 資料庫名稱
+
+$conn = sqlsrv_connect($serverName, array(
+    "Database" => $dbname,
+    "UID" => $username,
+    "PWD" => $password
+));
+
+// 檢查連線是否成功
+if ($conn) {
+    echo "Connected successfully"; // 連線成功時顯示訊息
+    // 在此可進行資料庫操作
+} else {
+    die(print_r(sqlsrv_errors(), true)); // 顯示連線錯誤訊息並停止程式執行
+}
 
 if ($conn) {
     // 如果表單提交，將值插入資料庫。
