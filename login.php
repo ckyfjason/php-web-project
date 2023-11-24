c8f83e2510a761e1a8023147e9b4ee4cc2389ad2
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-24 22:06:39 +0800

    .

--- login.php
+++ login.php
@@ -24,9 +24,34 @@ if ($conn) {
 } else {
     die(print_r(sqlsrv_errors(), true)); // 顯示連線錯誤訊息並停止程式執行
 }
+sqlsrv_close($conn); //?
 
-sqlsrv_close($conn);
+if (isset($_POST['username'])){
+    // removes backslashes
+$username = stripslashes($_REQUEST['username']);
+    //escapes special characters in a string
+$username = mysqli_real_escape_string($con,$username);
+$password = stripslashes($_REQUEST['password']);
+$password = mysqli_real_escape_string($con,$password);
+//Checking is user existing in the database or not
+    $query = "SELECT * FROM users WHERE username='$username'
+and password='".md5($password)."'";
+$result = mysqli_query($con,$query) or die(mysql_error());
+$rows = mysqli_num_rows($result);
+    if($rows==1){
+    $_SESSION['username'] = $username;
+        // Redirect user to index.php
+    header("Location: index.php");
+     }else{
+echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+}
+}else{
 ?>
+
+
+
 <div class="form">
 <h1>Log In</h1>
 <form action="" method="post" name="login">
@@ -37,7 +62,7 @@ sqlsrv_close($conn);
 </form>
 <p>Not registered yet? <a href='registration.php'>Register Here</a></p>
 </div>
-
+<?php } ?>
 
 </body>
 </html>
