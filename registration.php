ee57463e22ba00e3bb0e00584e9681df2cca26e1
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-24 22:23:28 +0800

    3

--- registration.php
+++ registration.php
@@ -6,36 +6,37 @@
 <body>
 <?php
 require('db.php');
-// If form submitted, insert values into the database.
-if (isset($_REQUEST['username'])){
-        // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
-        //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username); 
-	$email = stripslashes($_REQUEST['email']);
-	$email = mysqli_real_escape_string($con,$email);
-	$password = stripslashes($_REQUEST['password']);
-	$password = mysqli_real_escape_string($con,$password);
-	$trn_date = date("Y-m-d H:i:s");
-        $query = "INSERT into users (username, password, email, trn_date)
-VALUES ('$username', '".md5($password)."', '$email', '$trn_date')";
-        $result = mysqli_query($con,$query);
-        if($result){
-            echo "<div class='form'>
-<h3>You are registered successfully.</h3>
-<br/>Click here to <a href='login.php'>Login</a></div>";
-        }
-    }else{
+if (isset($_POST['submit'])){
+    $username = stripslashes($_POST['username']);
+    // 轉義特殊字符
+    $username = mysqli_real_escape_string($conn, $username); 
+    $email = stripslashes($_POST['email']);
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = stripslashes($_POST['password']);
+    $password = mysqli_real_escape_string($conn, $password);
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT); // 使用bcrypt進行密碼哈希
+    $trn_date = date("Y-m-d H:i:s");
+
+    $query = "INSERT into users (username, password, email, trn_date)
+              VALUES ('$username', '$hashedPassword', '$email', '$trn_date')";
+    $result = mysqli_query($connnn, $query);
+
+    if ($result){
+        echo "<div class='form'>
+              <h3>註冊成功。</h3>
+              <br/>點擊這裡<a href='login.php'>登錄</a></div>";
+    }
+} else {
 ?>
-<div class="form">
-<h1>Registration</h1>
-<form name="registration" action="" method="post">
-<input type="text" name="username" placeholder="Username" required />
-<input type="email" name="email" placeholder="Email" required />
-<input type="password" name="password" placeholder="Password" required />
-<input type="submit" name="submit" value="Register" />
-</form>
-</div>
+    <div class="form">
+        <h1>註冊</h1>
+        <form name="registration" action="" method="post">
+            <input type="text" name="username" placeholder="用戶名" required />
+            <input type="email" name="email" placeholder="電子郵件" required />
+            <input type="password" name="password" placeholder="密碼" required />
+            <input type="submit" name="submit" value="註冊" />
+        </form>
+    </div>
 <?php } ?>
 </body>
 </html>
