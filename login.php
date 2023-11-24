a789c472360c6a4a3b3e81b969416af114a128a9
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-25 00:44:25 +0800

    .final?

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
+    session_start();
     if(!isset($_SESSION['username']) || empty($_SESSION['username']) ) {
         header('Location: index.php');
-        exit;
+        exit();
     }
 ?>
 <!DOCTYPE html>
