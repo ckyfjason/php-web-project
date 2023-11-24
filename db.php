4f09feed9fa57b920529da928743fbca5dd1c525
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-24 21:37:33 +0800

    .

--- db.php
+++ db.php
@@ -3,8 +3,8 @@
 // I left password empty because i do not set password on localhost.
 $con = sqlsrv_connect("streamweb-dbserver.database.windows.net","ckyfjason","Database@password","streamweb-formaldb");
 // Check connection
-if (mysqli_connect_errno())
+/*if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . sqlsrv_connect_error();
-  }
+  }*/
 ?>
