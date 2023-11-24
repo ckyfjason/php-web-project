d54bfb82cba51e9ef06d5f54912d5c036879b77f
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-24 22:01:24 +0800

    my hope2

--- login.php
+++ login.php
@@ -1,6 +1,3 @@
-<?php
-$con = sqlsrv_connect("streamweb-dbserver.database.windows.net","ckyfjason","Database@password","streamweb-formaldb");
-?>
 <!DOCTYPE html>
 <html>
 <meta charset="utf-8">
@@ -9,11 +6,26 @@ $con = sqlsrv_connect("streamweb-dbserver.database.windows.net","ckyfjason","Dat
 </head>
 <body>
 <?php
-if (!$con) {
-    die("Connection failed");
-} else {
+$serverName = "streamweb-dbserver.database.windows.net"; // 資料庫伺服器名稱
+$username = "ckyfjason"; // 資料庫使用者名稱
+$password = "Database@password"; // 資料庫密碼
+$dbname = "streamweb-formaldb"; // 資料庫名稱
+
+$conn = sqlsrv_connect($serverName, array(
+    "Database" => $dbname,
+    "UID" => $username,
+    "PWD" => $password
+));
+
+// 檢查連線是否成功
+if ($conn) {
     echo "Connected successfully"; // 連線成功時顯示訊息
+    // 在此可進行資料庫操作
+} else {
+    die(print_r(sqlsrv_errors(), true)); // 顯示連線錯誤訊息並停止程式執行
 }
+
+sqlsrv_close($conn);
 ?>
 <div class="form">
 <h1>Log In</h1>
