af2dca35f51fedc107295870c1d384b402edd1ad
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-24 21:52:04 +0800

    .

--- login.php
+++ login.php
@@ -5,7 +5,16 @@
 <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-
+<?php
+$con = sqlsrv_connect("streamweb-dbserver.database.windows.net","ckyfjason","Database@password","streamweb-formaldb");
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully"; // 連線成功時顯示訊息
+    // 可以在這裡執行你的資料庫操作
+    // 例如查詢資料、新增資料等
+}
+?>
 <div class="form">
 <h1>Log In</h1>
 <form action="" method="post" name="login">
