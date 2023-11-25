a51d062f22a40763c4ab7a7590cb9971e2a135c5
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-26 02:33:05 +0800

    .

--- main/lobby.php
+++ main/lobby.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,14 +24,25 @@
        </div>
        
         <div id="nav__links">
-            <a class="nav__link" href="https://streamweb.azurewebsites.net"> 
-                Hub
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ede0e0" viewBox="0 0 24 24"><path d="M20 7.093v-5.093h-3v2.093l3 3zm4 5.907l-12-12-12 12h3v10h7v-5h4v5h7v-10h3zm-5 8h-3v-5h-8v5h-3v-10.26l7-6.912 7 6.99v10.182z"/></svg>
-            </a>
-            <a class="nav__link" id="create__room__btn" href="lobby.html">
-                創建房間
-               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ede0e0" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
-            </a>
+        <?php
+            if(empty($_SESSION['username']) ) { echo//當用戶沒有登入時
+                '<a class="hub__nav__link" href="./registration.php">
+                Register
+                </a>
+                <a class="hub__nav__link" href="./login.php">
+                Login
+                </a>';
+            } else { echo //當用已登入時
+                '<a class="hub__nav__link"> Hi, '. $_SESSION['username'] .'!</a>
+                <a class="hub__nav__link" href="./logout.php">
+                Logout
+                </a>';
+            }
+            ?>
+             <a class="nav__link" id="create__room__btn" href="./main/lobby.html">
+                 已在創建
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ede0e0" viewBox="0 0 24 24"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm6 13h-5v5h-2v-5h-5v-2h5v-5h2v5h5v2z"/></svg>
+             </a>
         </div>
         
     </header>
