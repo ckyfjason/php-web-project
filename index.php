16e61779115bf41f23beb1f43f3d39967d151b59
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-11-26 23:39:25 +0800

    .

--- index.php
+++ index.php
@@ -27,15 +27,15 @@
          <div id="nav__links">
             <?php
             if(empty($_SESSION['username']) ) { echo//當用戶沒有登入時
-                '<a id="hub__nav__link" href="./registration.php">
+                '<a class="hub__nav__link" href="./registration.php">
                 Register
                 </a>
-                <a id="hub__nav__link" href="./login.php">
+                <a class="hub__nav__link" href="./login.php">
                 Login
                 </a>';
             } else { echo //當用已登入時
                 '<a id="login__imformation"> Hi, '. $_SESSION['username'] .'!</a>
-                <a id="hub__nav__link" href="./logout.php">
+                <a class="hub__nav__link" href="./logout.php">
                 Logout
                 </a>';
             }
