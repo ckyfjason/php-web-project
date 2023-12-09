c83e685276e394e9284b9c19337242e0cbf66a96
ckyfjason <96482679+ckyfjason@users.noreply.github.com>
2023-12-10 01:45:54 +0800

    99

--- index2.php
+++ index2.php
@@ -1,10 +1,12 @@
 <?php
-if (isset($_POST["name"])) {
-    echo "Hello world, " . $_POST["name"] . "!";
-    // 如果需要返回數據給前端，可以使用 JSON 格式，例如：
-    echo json_encode(["message" => "Hello world, " . $_POST["name"] . "!"]);
-} else {
-    // 如果沒有收到名字，你可以返回一個錯誤訊息或者其他內容
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST["name"])) {
+        echo "Hello world, " . $_POST["name"] . "!";
+        // 如果需要返回數據給前端，可以使用 JSON 格式，例如：
+        // echo json_encode(["message" => "Hello world, " . $_POST["name"] . "!"]);
+    } else {
+        // 如果沒有收到名字，你可以返回一個錯誤訊息或者其他內容
+    }
 }
 ?>
 
@@ -16,16 +18,18 @@ if (isset($_POST["name"])) {
 <body>
     <div id='test'></div>
     <script>
-        function sayHello() {
-            $.post('index2.php', {
-                name: "Jacky",
-                number: "1"
-            }, function(response) {
-                //$('#test').html(response); // 將伺服器端的回應設置為 #test 元素的文字內容
+        $(document).ready(function() {
+            $('#sayHelloBtn').click(function() {
+                $.post('', {  // 空字符串表示向當前頁面發送 POST 請求
+                    name: "Jacky",
+                    number: "1"
+                }, function(response) {
+                    $('#test').text(response); // 將伺服器端的回應設置為 #test 元素的文字內容
+                });
             });
-        };
+        });
     </script>
-    <button id="sayHelloBtn" onclick="sayHello()">Say hello</button>
+    <button id="sayHelloBtn">Say hello</button>
     <section id="message"></section>
 </body>
 </html>
